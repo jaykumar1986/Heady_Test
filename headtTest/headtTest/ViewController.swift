e9667776510b1fdcec71a7e2506029acd1825a97
Jay Kumar <jay.kumar@dnata.com>
2018-07-10 09:27:47 +0400

    Adding xcode temp

---
+++ headtTest/headtTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  headtTest
+//
+//  Created by Jay Kumar on 7/10/18.
+//  Copyright © 2018 Jay Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
